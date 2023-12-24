290112c565ed2c673cf896ce21031d2f8d70e4ff
LockFord55 <lockford55@gmail.com>
2023-12-24 20:09:06 +0300

    Лабораторная №4. Перед изменением cart.html.

--- store/views.py
+++ store/views.py
@@ -26,9 +26,8 @@ def products_view(request):
 
 def shop_view(request):
     if request.method == "GET":
-        with open('store/shop.html', encoding='utf-8') as f:
-            data = f.read()
-        return HttpResponse(data)
+        return render(request, 'store/shop.html',
+                      context={"products": DATABASE.values()})
 
 
 def products_page_view(request, page):
@@ -51,8 +50,16 @@ def products_page_view(request, page):
 def cart_view(request):
     if request.method == "GET":
         data = view_in_cart()
-        return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
-                                                     'indent': 4})
+        if request.GET.get('format') == 'JSON':
+            return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
+                                                         'indent': 4})
+        products = []  # Список продуктов
+        for product_id, quantity in data['products'].items():
+            product = DATABASE.get(product_id)
+            product['quantity'] = quantity
+            product["price_total"] = f"{quantity * product['price_after']:.2f}"
+            products.append(product)
+        return render(request, "store/cart.html", context={"products": products})
 
 
 def cart_add_view(request, id_product):
